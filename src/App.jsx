ef97c4600bb9af0f0f43046ac88a99e051f5e713
Yeshwanth <vryeshwanth06@gmail.com>
2025-11-30 20:32:42 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
 
 export default function App() {
-    return <Navbar />;
+    return (
+        <div className="w-full">
+            <Navbar />
+            <div className="w-full">
+                <Home />
+            </div>
+        </div>
+    );
 }
