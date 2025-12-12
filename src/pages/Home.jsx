b96a22ebccd1e25a2bbbe2ebf64dbfb549061fac
Yeshwanth <vryeshwanth06@gmail.com>
2025-12-12 19:17:47 +0530

    Update animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,19 @@ export default function Home() {
     return (
         <section
             id="home"
-            className="min-h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-4 dark:bg-neutral-950 dark:text-neutral-100"
+            className="min-h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-4 dark:bg-neutral-950 dark:text-neutral-100 animate-fade-in"
         >
-            <h1 className="text-3xl md:text-4xl animate-fade-in-200 text-center">
-                Hi , I'm
-            </h1>
-            <span className="text-5xl md:text-7xl font-bold animate-fade-in-300 text-center">
+            <h1 className="text-3xl md:text-4xl text-center">Hi , I'm</h1>
+            <span className="text-5xl md:text-7xl font-bold  text-center">
                 V R Yeshwanth
             </span>
-            <h2 className="text-xl md:text-3xl animate-fade-in-400 text-center">
+            <h2 className="text-xl md:text-3xl  text-center">
                 Aspiring Full Stack Developer
             </h2>
-            <p className="text-lg md:text-2xl animate-fade-in-500 text-center">
+            <p className="text-lg md:text-2xl  text-center">
                 Turning ideas into clean and functional UI.
             </p>
-            <div className="animate-fade-in-600 flex gap-4">
+            <div className=" flex gap-4">
                 <a href="https://github.com/VRYeshwanth">
                     <i className="bx bxl-github text-3xl px-2 py-1 rounded-full cursor-pointer text-black bg-white hover:text-white hover:bg-neutral-800 transition duration-300 dark:text-white dark:bg-neutral-950 dark:hover:bg-neutral-700"></i>
                 </a>
@@ -24,7 +22,7 @@ export default function Home() {
                     <i className="bx bxl-linkedin text-3xl px-2 py-1 rounded-full cursor-pointer text-black bg-white hover:text-white hover:bg-neutral-800 transition duration-300 dark:text-white dark:bg-neutral-950 dark:hover:bg-neutral-700"></i>
                 </a>
             </div>
-            <div className="animate-fade-in-700 flex gap-8">
+            <div className="flex gap-8">
                 <a
                     href="#projects"
                     className="text-lg md:text-xl px-2 md:px-4 py-1 md:py-2 rounded-xl text-black bg-white border border-neutral-700 hover:bg-neutral-200 transition duration-300 dark:text-white dark:bg-neutral-950 dark:hover:bg-neutral-700 cursor-pointer"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,17 +6,12 @@ module.exports = {
         extend: {
             keyframes: {
                 "fade-in": {
-                    "0%": { opacity: 0 },
-                    "100%": { opacity: 1 },
+                    "0%": { opacity: 0, transform: "translateY(30px)" },
+                    "100%": { opacity: 1, transform: "translateY(0)" },
                 },
             },
             animation: {
-                "fade-in-200": "fade-in 1s ease-in 200ms",
-                "fade-in-300": "fade-in 1s ease-in 300ms",
-                "fade-in-400": "fade-in 1s ease-in 400ms",
-                "fade-in-500": "fade-in 1s ease-in 500ms",
-                "fade-in-600": "fade-in 1s ease-in 600ms",
-                "fade-in-700": "fade-in 1s ease-in 700ms",
+                "fade-in": "fade-in 0.8s ease-out forwards",
             },
         },
     },
